52a8ba0063b6fa0bd03133e0fab02c98c5486977
webWizrd <superultradev000@gmail.com>
2022-11-09 14:39:40 +0800

    First..

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,13 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
-    </div>
-  )
+  return <div>Hi</div>;
 };
 
 export default Home;
