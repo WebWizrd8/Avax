fc7f06492f2b870831884cd7d0e46191719544b1
webWizrd <superultradev000@gmail.com>
2022-11-09 14:56:03 +0800

    Higher order component

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { PageHOC } from "../components";
 
 const Home = () => {
-  return <div>Hi</div>;
+  return <div></div>;
 };
 
-export default Home;
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>
+);
